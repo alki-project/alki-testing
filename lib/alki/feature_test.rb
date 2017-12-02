a7ae77a8c62bece6d92adfe265d3fd3dadaec7f3
Matt Edlefsen <matt.edlefsen@gmail.com>
2017-12-02 09:40:56 -0500

    Add ability to easily extend feature specs
    Version 0.3.0

--- lib/alki/feature_test.rb
+++ lib/alki/feature_test.rb
@@ -1,5 +1,24 @@
 require 'alki/test'
 
+class Alki::FeatureSpec < Minitest::Spec
+end
+
+Minitest::Spec.register_spec_type Alki::FeatureSpec do |desc|
+  desc.is_a? String
+end
+
+class << Alki::Test
+  def feature_exec(&blk)
+    Alki::FeatureSpec.class_exec(&blk)
+  end
+
+  def app(&blk)
+    feature_exec do
+      define_method(:app,&blk)
+    end
+  end
+end
+
 feature_test_helper = File.join(Alki::Test.tests_root,'feature_test_helper.rb')
 
 if File.exist? feature_test_helper
