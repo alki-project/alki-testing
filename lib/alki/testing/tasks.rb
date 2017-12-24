8424a4fb7507d2d23552d41bfbb54e961ce0cbd6
Matt Edlefsen <matt.edlefsen@gmail.com>
2017-12-23 23:32:40 -0500

    Run tests in subdirectories

--- lib/alki/testing/tasks.rb
+++ lib/alki/testing/tasks.rb
@@ -2,24 +2,24 @@ require 'rake/testtask'
 
 Rake::TestTask.new do |t|
   t.name = 'test:unit'
-  t.pattern = "test/unit/*_test.rb"
+  t.pattern = "test/unit/**/*_test.rb"
   t.warning = false
 end
 
 Rake::TestTask.new do |t|
   t.name = 'test:feature'
-  t.pattern = "test/feature/*_test.rb"
+  t.pattern = "test/feature/**/*_test.rb"
   t.warning = false
 end
 
 Rake::TestTask.new do |t|
   t.name = 'test:integration'
-  t.pattern = "test/integration/*_test.rb"
+  t.pattern = "test/integration/**/*_test.rb"
   t.warning = false
 end
 
 Rake::TestTask.new do |t|
   t.name = 'test'
-  t.pattern = "test/*/*_test.rb"
+  t.pattern = "test/**/*_test.rb"
   t.warning = false
 end
